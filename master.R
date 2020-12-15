1779b852966232eb8711f106bb3d00066b8567d1
MAB <belairma@gmail.com>
2020-12-15 18:58:36 -0500

    Updated for data to December 14, 2020.

--- master.R
+++ master.R
@@ -28,7 +28,7 @@ GAMMA = 1/4
 filename <- paste0("./graphs/bc-covid-epi-rt-",Sys.Date(),".PNG")
 date.today <- Sys.Date()
 date.yesterday <- Sys.Date() -1
-count.yesterday <- 759
+count.yesterday <- 522
 
 covid.raw <- read_csv("http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv")
 
@@ -41,9 +41,9 @@ covid.daily_count <- covid.raw %>%
   slice(1:(n()-1)) %>%
   add_row(Reported_Date = date.yesterday, n=count.yesterday) %>%
   pad(start_val = ymd("2020-01-26"), end_val = date.yesterday) %>%
-  mutate(n=replace_na(n,0)) %>%
-  mutate(n=replace(n, which(Reported_Date==ymd("2020-12-12")), 698)) %>% 
-  mutate(n=replace(n, which(Reported_Date==ymd("2020-12-11")), 689))
+  mutate(n=replace_na(n,0)) #%>%
+  #mutate(n=replace(n, which(Reported_Date==ymd("2020-12-12")), 698)) %>% 
+  #mutate(n=replace(n, which(Reported_Date==ymd("2020-12-11")), 689))
 
 
 ### Process, and estimate daily rt  
